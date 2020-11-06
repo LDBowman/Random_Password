03491e3798d8aacaa24b6e8a8fd4c8dae85c4ee0
ldbow <ldbowmn@gmail.com>
2020-11-06 15:42:55 -0600

    Created JS Questions (Prompt & Confirms)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 var generateBtn = document.querySelector("#generate");
+
+function genPass(){
+    var specialC = confirm("Want to include Special Characters?");
+    var upperC = confirm("Want to include Uppercase Letters?");
+    var lowerC = confirm("Want to include Lowercase Letters?");
+    var numbers = confirm("Want to include numbers?");
+    var length = prompt("Enter Password Character Length");
+}
+
+generateBtn.addEventListener("click", password);
+function password() {
+    var password = genPass();
+}
